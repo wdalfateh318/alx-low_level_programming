49c6ef62b5ffd1d2621409a4fadd544d6a4500f0
mohammed <wdalfateh318@gmail.com>
2023-09-12 14:02:19 -0700

    mo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - check the code for ALX School students.
- *
- * Return: Always 0.
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-
-void print_alphabet(void)
+int main(void)
 {
-	char alp = 'a';
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	while (alp <= 'z')
-	{
-		_putchar (alp);
-		alp++;
-
-	}
-	_putchar ('\n');
-
+	return (0);
 }
