b159221e5516ac3d43e5bdaf75191ffaf9b37b8d
mohammed <wdalfateh318@gmail.com>
2023-09-21 19:12:56 -0700

    moe

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strcat - concatenates two strings
@@ -28,4 +29,17 @@ char *_strcat(char *dest, char *src)
 
 	return (dest);
 }
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
 
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
+}
